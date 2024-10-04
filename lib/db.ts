61091311715e83b00cc3f3dfd173abe25be3b46c
tuanna-kite <tuanna.kiteacademy@gmail.com>
2024-10-04 19:33:13 +0700

    fix eslint

--- lib/db.ts
+++ lib/db.ts
@@ -1,12 +1,11 @@
 import { PrismaClient } from '@prisma/client';
-
 declare global {
   let prisma: PrismaClient | undefined;
 }
 
-export const db: PrismaClient = globalThis.prisma || new PrismaClient();
+export const db: PrismaClient = new PrismaClient();
 
 // If we're not in production, let's save `db` to the global object to prevent crashes
-if (process.env.NODE_ENV !== 'production') {
-  globalThis.prisma = db;
-}
+// if (process.env.NODE_ENV !== 'production') {
+//   globalThis.prisma = db;
+// }

6597bce74f7e78dd85e9a9830a5fd6e8a916d1a1
tuanna-kite <tuanna.kiteacademy@gmail.com>
2024-10-04 19:52:55 +0700

    remove loading

--- app/error.tsx
+++ app/error.tsx
@@ -1,7 +1,8 @@
+'use client';
 import React from 'react';
 
 const ErrorBoundary = () => {
-  return <div className='text-2xl py-20 text-center'>Đã có lỗi xảy ra</div>;
+  return <div className='text-2xl py-20 text-center'>Error</div>;
 };
 
 export default ErrorBoundary;
